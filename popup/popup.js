5663b01a1e79b5904d2079ae50da7c8147c4e13f
Lukáš Drahník <ldrahnik@gmail.com>
2022-03-08 08:08:08 +0100

    Addded Debug: TestovanyZdravotniPojistovnaKod

--- popup/popup.js
+++ popup/popup.js
@@ -156,11 +156,11 @@ function tryToSloucitForeignProfiles(index, ZadankaData, SlucujiciProfil, Slucov
                     xhrSlucovanyProfil.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
                     xhrSlucovanyProfil.onreadystatechange = function() {
                         if(xhrSlucovanyProfil.readyState === XMLHttpRequest.DONE) {
-                                
+
                             console.log("DEBUG: Zadanka: " + ZadankaData.TestovanyNarodnostKod, ZadankaData.TestovanyNarodnostNazev + ", PacientInfo NarodnostKod a RobObcanstviZemeKod: " + SlucovanyProfil.Pacient_NarodnostKod, SlucovanyProfil.Pacient_RobObcanstviZemeKod);
 
                             if(SlucovanyProfil.PacientDatumNarozeniText.startsWith("1.") && ZadankaData.TestovanyDatumNarozeniText != SlucovanyProfil.PacientDatumNarozeniText) {
-                                console.log("DEBUG: Datum narození pacient: " + SlucovanyProfil.PacientDatumNarozeniText, ", žádanka: " + ZadankaData.TestovanyDatumNarozeniText);   
+                                console.log("DEBUG: Datum narození pacient: " + SlucovanyProfil.PacientDatumNarozeniText, ", žádanka: " + ZadankaData.TestovanyDatumNarozeniText, ", pojistovna: " + ZadankaData.TestovanyZdravotniPojistovnaKod);   
                             }
 
                             // podařilo se sloučit?
